2a37244519aff0b8a585189c4137ef1feab0cae4
QuangVann <115283300+QuangVann@users.noreply.github.com>
2023-03-09 01:43:47 +0700

    up

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import Html from "../../img/HTML5.png";
+import CSS from "../../img/CSS3.png";
+import javaScript from "../../img/amazon.png";
+import javaScript from "../../img/javaScript.png";
+import Reacts from "../../img/React.png";
+import Facebook from "../../img/Facebook.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Works for All these
+          </span>
+          <span>Brands & Clients</span>
+          <spane>
+            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+          </spane>
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={Html} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={CSS} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={javaScript} alt="" />
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={React} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
